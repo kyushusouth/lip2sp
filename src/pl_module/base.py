59848aa44c112ca8c9d3a040ade68237c1be2e21
kyushusouth <minami.taisuke.326@s.kyushu-u.ac.jp>
2024-03-17 05:23:38 +0900

    implement logging

--- src/pl_module/base.py
+++ src/pl_module/base.py
@@ -1,23 +1,35 @@
-import pathlib
+from pathlib import Path
 
 import lightning as L
+import MeCab
 import numpy as np
+import omegaconf
+import pandas as pd
 import torch
+import wandb
+import whisper
 from cosine_annealing_warmup import CosineAnnealingWarmupRestarts
-from scipy.io.wavfile import write
+from jiwer import wer
+from torchmetrics.audio.pesq import PerceptualEvaluationSpeechQuality
+from torchmetrics.audio.stoi import ShortTimeObjectiveIntelligibility
 
 from src.loss_fn.base import LossFunctions
 from src.model.base import BaseModel
+from src.model.pwg import Generator
 
 
 class LitBaseModel(L.LightningModule):
-    def __init__(self, cfg) -> None:
+    def __init__(self, cfg: omegaconf.DictConfig) -> None:
         super().__init__()
         self.cfg = cfg
         self.learning_rate = cfg["training"]["optimizer"]["learning_rate"]
         self.automatic_optimization = True
         self.model = BaseModel(cfg)
         self.loss_fn = LossFunctions()
+
+        self.pwg = Generator(cfg)
+        pretrained_dict = torch.load(cfg["model"]["pwg"]["model_path"])
+        self.pwg.load_state_dict(pretrained_dict["gen"], strict=True)
 
     def forward(
         self,
@@ -34,7 +46,7 @@ class LitBaseModel(L.LightningModule):
         )
         return pred
 
-    def step_typical_process(self, batch: list):
+    def step_typical_process(self, batch: list) -> tuple:
         (
             wav,
             lip,
@@ -55,7 +67,7 @@ class LitBaseModel(L.LightningModule):
         loss = self.loss_fn.mae_loss(pred, feature, feature_len, max_len=pred.shape[-1])
         return pred, loss
 
-    def training_step(self, batch: list, batch_index: int):
+    def training_step(self, batch: list, batch_index: int) -> torch.Tensor:
         pred, loss = self.step_typical_process(batch)
         self.log(
             "train_loss",
@@ -65,7 +77,7 @@ class LitBaseModel(L.LightningModule):
         )
         return loss
 
-    def validation_step(self, batch: list, batch_index: int):
+    def validation_step(self, batch: list, batch_index: int) -> None:
         pred, loss = self.step_typical_process(batch)
         self.log(
             "val_loss",
@@ -74,40 +86,227 @@ class LitBaseModel(L.LightningModule):
             batch_size=self.cfg["training"]["params"]["batch_size"],
         )
 
-    # def test_step(self, batch: list, batch_index: int):
-    #     (
-    #         wav,
-    #         lip,
-    #         feature,
-    #         feature_avhubert,
-    #         spk_emb,
-    #         feature_len,
-    #         lip_len,
-    #         speaker,
-    #         filename,
-    #     ) = batch
-    #     pred = self.forward(
-    #         lip=lip,
-    #         audio=None,
-    #         lip_len=lip_len,
-    #         spk_emb=spk_emb,
-    #     )
-    #     noise = torch.randn(
-    #         pred.shape[0], 1, pred.shape[-1] * self.cfg["data"]["audio"]["hop_length"]
-    #     ).to(device=pred.device, dtype=pred.dtype)
-    #     wav_pred = self.pwg(noise, pred)
-    #     wav_abs = self.pwg(noise, feature)
-    #     n_sample_min = min(wav.shape[-1], wav_pred.shape[-1], wav_abs.shape[-1])
+    def test_step(self, batch: list, batch_index: int) -> None:
+        (
+            wav_gt,
+            lip,
+            feature,
+            feature_avhubert,
+            spk_emb,
+            feature_len,
+            lip_len,
+            speaker,
+            filename,
+        ) = batch
+        pred = self.forward(
+            lip=lip,
+            audio=None,
+            lip_len=lip_len,
+            spk_emb=spk_emb,
+        )
+        noise = torch.randn(
+            pred.shape[0], 1, pred.shape[-1] * self.cfg["data"]["audio"]["hop_length"]
+        ).to(device=pred.device, dtype=pred.dtype)
+        wav_pred = self.pwg(noise, pred)
+        wav_abs = self.pwg(noise, feature)
+        n_sample_min = min(wav_gt.shape[-1], wav_pred.shape[-1], wav_abs.shape[-1])
 
-    # def save_wav(
-    #     self, wav: torch.Tensor, n_sample: int, save_path: pathlib.Path | str
-    # ) -> None:
-    #     wav = wav.squeeze(0).squeeze(0)
-    #     wav = wav.cpu().detach().numpy()
-    #     wav /= np.max(np.abs(wav))
-    #     wav = wav.astype(np.float32)
-    #     wav = wav[:n_sample]
-    #     write(str(save_path), rate=self.cfg["data"]["audio"]["sr"], data=wav)
+        wav_gt = self.process_wav(wav_gt, n_sample_min)
+        wav_abs = self.process_wav(wav_abs, n_sample_min)
+        wav_pred = self.process_wav(wav_pred, n_sample_min)
+
+        pesq_abs = self.wb_pesq_evaluator(wav_abs, wav_gt)
+        pesq_pred = self.wb_pesq_evaluator(wav_pred, wav_gt)
+        stoi_abs = self.stoi_evaluator(wav_abs, wav_gt)
+        stoi_pred = self.stoi_evaluator(wav_pred, wav_gt)
+        estoi_abs = self.estoi_evaluator(wav_abs, wav_gt)
+        estoi_pred = self.estoi_evaluator(wav_pred, wav_gt)
+
+        utt = None
+        for i, row in self.df_utt.iterrows():
+            if str(row["utt_num"]) in filename[0]:
+                utt = row["text"].replace("。", "").replace("、", "")
+                break
+        if utt is None:
+            raise ValueError("Utterance was not found.")
+
+        utt_recog_gt = (
+            self.speech_recognizer.transcribe(
+                wav_gt.to(dtype=torch.float32), language="ja"
+            )["text"]
+            .replace("。", "")
+            .replace("、", "")
+        )
+        utt_recog_abs = (
+            self.speech_recognizer.transcribe(
+                wav_abs.to(dtype=torch.float32), language="ja"
+            )["text"]
+            .replace("。", "")
+            .replace("、", "")
+        )
+        utt_recog_pred = (
+            self.speech_recognizer.transcribe(
+                wav_pred.to(dtype=torch.float32), language="ja"
+            )["text"]
+            .replace("。", "")
+            .replace("、", "")
+        )
+
+        utt_parse = self.mecab.parse(utt)
+        utt_recog_gt_parse = self.mecab.parse(utt_recog_gt)
+        utt_recog_abs_parse = self.mecab.parse(utt_recog_abs)
+        utt_recog_pred_parse = self.mecab.parse(utt_recog_pred)
+
+        wer_gt = self.calc_error_rate(utt_parse, utt_recog_gt_parse)
+        wer_abs = self.calc_error_rate(utt_parse, utt_recog_abs_parse)
+        wer_pred = self.calc_error_rate(utt_parse, utt_recog_pred_parse)
+
+        data = [
+            [
+                speaker[0],
+                filename[0],
+                "gt",
+                wandb.Audio(wav_gt.cpu(), sample_rate=self.cfg["data"]["audio"]["sr"]),
+                None,
+                None,
+                None,
+                wer_gt,
+            ],
+            [
+                speaker[0],
+                filename[0],
+                "abs",
+                wandb.Audio(wav_abs.cpu(), sample_rate=self.cfg["data"]["audio"]["sr"]),
+                pesq_abs,
+                stoi_abs,
+                estoi_abs,
+                wer_abs,
+            ],
+            [
+                speaker[0],
+                filename[0],
+                "pred",
+                wandb.Audio(
+                    wav_pred.cpu(), sample_rate=self.cfg["data"]["audio"]["sr"]
+                ),
+                pesq_pred,
+                stoi_pred,
+                estoi_pred,
+                wer_pred,
+            ],
+        ]
+        self.test_data_list += data
+
+    def process_wav(self, wav: torch.Tensor, n_sample: int) -> torch.Tensor:
+        wav = wav.to(torch.float32)
+        wav = wav.squeeze(0).squeeze(0)
+        wav /= torch.max(torch.abs(wav))
+        wav = wav[:n_sample]
+        return wav
+
+    def calc_error_rate(self, utt: list, utt_pred: list) -> float:
+        wer_gt = None
+        try:
+            wer_gt = np.clip(wer(utt, utt_pred), a_min=0, a_max=1)
+        except:  # noqa: E722
+            wer_gt = 1.0
+        if wer_gt is None:
+            raise ValueError("Word Error Rate was not found.")
+        return wer_gt
+
+    def on_test_start(self) -> None:
+        self.test_data_columns = [
+            "speaker",
+            "filename",
+            "kind",
+            "wav",
+            "pesq",
+            "stoi",
+            "estoi",
+            "wer",
+        ]
+        self.test_data_list: list[list] = []
+
+        csv_path = Path("~/lip2sp/csv/ATR503.csv").expanduser()
+        self.df_utt = pd.read_csv(str(csv_path))
+        self.df_utt = self.df_utt.drop(columns=["index"])
+        self.df_utt = self.df_utt.loc[self.df_utt["subset"] == "j"]
+
+        self.wb_pesq_evaluator = PerceptualEvaluationSpeechQuality(
+            self.cfg["data"]["audio"]["sr"], "wb"
+        )
+        self.stoi_evaluator = ShortTimeObjectiveIntelligibility(
+            self.cfg["data"]["audio"]["sr"], extended=False
+        )
+        self.estoi_evaluator = ShortTimeObjectiveIntelligibility(
+            self.cfg["data"]["audio"]["sr"], extended=True
+        )
+        self.speech_recognizer = whisper.load_model("large")
+        self.mecab = MeCab.Tagger("-Owakati")
+
+    def on_test_end(self) -> None:
+        table = wandb.Table(columns=self.test_data_columns, data=self.test_data_list)
+        wandb.log({"test_data": table})
+
+        kind_index = self.test_data_columns.index("kind")
+        pesq_index = self.test_data_columns.index("pesq")
+        stoi_index = self.test_data_columns.index("stoi")
+        estoi_index = self.test_data_columns.index("estoi")
+        wer_index = self.test_data_columns.index("wer")
+
+        result: dict[str, dict[str, list]] = {
+            "gt": {"pesq": [], "stoi": [], "estoi": [], "wer": []},
+            "abs": {"pesq": [], "stoi": [], "estoi": [], "wer": []},
+            "pred": {"pesq": [], "stoi": [], "estoi": [], "wer": []},
+        }
+
+        for test_data in self.test_data_list:
+            kind = test_data[kind_index]
+            pesq = test_data[pesq_index]
+            stoi = test_data[stoi_index]
+            estoi = test_data[estoi_index]
+            wer = test_data[wer_index]
+            result[kind]["pesq"].append(pesq)
+            result[kind]["stoi"].append(stoi)
+            result[kind]["estoi"].append(estoi)
+            result[kind]["wer"].append(wer)
+
+        columns = ["kind", "pesq", "stoi", "estoi", "wer"]
+        data_list = []
+        for kind, value_dict in result.items():
+            if kind == "gt":
+                data_list.append(
+                    [
+                        kind,
+                        None,
+                        None,
+                        None,
+                        np.mean(value_dict["wer"]),
+                    ]
+                )
+            else:
+                data_list.append(
+                    [
+                        kind,
+                        np.mean(value_dict["pesq"]),
+                        np.mean(value_dict["stoi"]),
+                        np.mean(value_dict["estoi"]),
+                        np.mean(value_dict["wer"]),
+                    ]
+                )
+        table = wandb.Table(columns=columns, data=data_list)
+        wandb.log({"metrics_mean": table})
+
+        del (
+            self.test_data_columns,
+            self.test_data_list,
+            self.df_utt,
+            self.wb_pesq_evaluator,
+            self.stoi_evaluator,
+            self.estoi_evaluator,
+            self.speech_recognizer,
+            self.mecab,
+        )
 
     def configure_optimizers(self):
         optimizer = None
