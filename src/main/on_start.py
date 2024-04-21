3679d631d75ec39790daa0931865d1b91a17faf7
kyushusouth <minami.taisuke.326@s.kyushu-u.ac.jp>
2024-04-21 18:46:36 +0900

    reloginする必要がないのでコメントアウト

--- src/main/on_start.py
+++ src/main/on_start.py
@@ -8,6 +8,6 @@ from lightning.pytorch import seed_everything
 
 torch.set_float32_matmul_precision("medium")
 dotenv.load_dotenv(dotenv_path="../.env")
-wandb.login(key=os.environ["WANDB_API_KEY"])
+# wandb.login(key=os.environ["WANDB_API_KEY"])
 CURRENT_TIME = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
 seed_everything(seed=42)
