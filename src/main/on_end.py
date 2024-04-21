39b8e4d80930dd718246483ab1f7037fa501bf15
kyushusouth <minami.taisuke.326@s.kyushu-u.ac.jp>
2024-04-21 11:16:46 +0900

    mainスクリプト終了時に実行するスクリプト

---
+++ src/main/on_end.py
@@ -0,0 +1,15 @@
+import os
+from pathlib import Path
+
+
+def rename_checkpoint_file(checkpoint_dir: Path | str) -> None:
+    if not isinstance(checkpoint_dir, Path):
+        checkpoint_dir = Path(checkpoint_dir)
+    checkpoint_path_list = list(checkpoint_dir.glob("*.ckpt"))
+    for checkpoint_path in checkpoint_path_list:
+        dest_path = Path(
+            str(checkpoint_path).replace(
+                checkpoint_path.stem, checkpoint_path.stem.replace("=", ":")
+            )
+        )
+        os.rename(str(checkpoint_path), str(dest_path))
