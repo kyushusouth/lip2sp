05b6d7b88b0c7fb3f2e2e178edce123d67d571be
kyushusouth <minami.taisuke.326@s.kyushu-u.ac.jp>
2024-09-19 11:48:43 +0900

    update

--- src/data_process/layerwise_analysis/prepare_forced_alienment_files.py
+++ src/data_process/layerwise_analysis/prepare_forced_alienment_files.py
@@ -4,12 +4,16 @@ montreal forced alignerによるforced alignmentを行う場合、condaの仮想
 mfa align --overwrite --clean --use_mp /home/minami/dataset/lip/forced_alignment_results/data japanese_mfa japanese_mfa /home/minami/dataset/lip/forced_alignment_results/results
 """
 
-from pathlib import Path
 import shutil
+from pathlib import Path
+
+import hydra
+import omegaconf
+import polars as pl
 from tqdm import tqdm
 
 
-def main():
+def kablab():
     text_dir = Path("/home/minami/dataset/lip/utt")
     audio_dir = Path("/home/minami/dataset/lip/wav")
     save_dir = Path("/home/minami/dataset/lip/forced_alignment_results/data")
@@ -24,6 +28,27 @@ def main():
             save_dir_spk.mkdir(parents=True, exist_ok=True)
             shutil.copy(str(audio_path), str(save_dir_spk / audio_path.name))
             shutil.copy(str(text_path), str(save_dir_spk / f"{text_path.stem}.lab"))
+
+
+@hydra.main(version_base=None, config_path="../../../conf", config_name="config")
+def main(cfg: omegaconf.DictConfig) -> None:
+    audio_dir = Path(cfg.path.jvs.audio_dir).expanduser()
+    text_dir = Path(cfg.path.jvs.text_dir).expanduser()
+    save_dir = Path(cfg.path.jvs.forced_alignment_results_dir).expanduser()
+    df = pl.read_csv(str(Path(cfg.path.jvs.df_path).expanduser()))
+
+    for row in df.iter_rows(named=True):
+        utt = (
+            pl.read_csv(
+                str(text_dir / row["speaker"] / row["data"] / "transcripts_utf8.txt"),
+                has_header=False,
+                separator=":",
+            )
+            .rename({"column_1": "filename", "column_2": "utt"})
+            .filter(pl.col("filename") == row["filename"])
+            .select("utt")
+            .to_numpy()[0][0]
+        )
 
 
 if __name__ == "__main__":
