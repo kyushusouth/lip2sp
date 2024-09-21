67d40496422bf7573a2ce3887301af8ecf303d61
kyushusouth <minami.taisuke.326@s.kyushu-u.ac.jp>
2024-09-21 17:26:37 +0900

    update

--- src/run/utils.py
+++ src/run/utils.py
@@ -3,10 +3,12 @@ from pathlib import Path
 from src.main.on_end import rename_checkpoint_file
 
 
-def get_last_checkpoint_path(checkpoint_dir: Path) -> Path:
+def get_last_checkpoint_path(checkpoint_dir: Path) -> Path | None:
     rename_checkpoint_file(checkpoint_dir)
     checkpoint_path_list = list(checkpoint_dir.glob("**/*.ckpt"))
     checkpoint_path_list = sorted(
         checkpoint_path_list, key=lambda x: x.parent.name, reverse=False
     )
+    if len(checkpoint_path_list) == 0:
+        return None
     return checkpoint_path_list[-1]
